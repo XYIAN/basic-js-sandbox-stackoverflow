f91cfb92172d03e9efe54205c758cbc688b43184
XYIAN <kylexyiandilbeck@gmail.com>
2024-01-01 22:25:26 -0800

    update new sandbox examples

---
+++ src/components/example/MapExample.jsx
@@ -0,0 +1,24 @@
+export default function RadioButtonGroup({ insurances, isLoaded }) {
+  if (!isLoaded) {
+    return <h5>Loading...</h5>;
+  } else
+    return (
+      <>
+        {insurances.map((element) => (
+          <div id={`radioDiv_${element.id_in_group}`}>
+            <input
+              type="radio"
+              id={`button_${element.id_in_group}`}
+              name="insGroup"
+            />
+            <label for={`button_${element.id_in_group}`}>
+              {element.insurance_name}
+            </label>
+            <br />
+          </div>
+        ))}
+        <input type="radio" id="notListed" name="insGroup" />
+        <label for="notListed">My insurance is not listed.</label>
+      </>
+    );
+}
